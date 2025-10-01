c129df5aaeeaad2e98f7802f5d067a38968e2cc1
noorsyed16 <noorsyed464@gmail.com>
2025-09-30 18:35:57 -0600

    Added Honeypot to migrate Netlify Forms to new version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,8 +19,40 @@ export default function Contact() {
         <input type="email" name="email" placeholder="Email" required />
         <textarea name="message" placeholder="Message" rows="5" required />
         <button type="submit" className="contactButton">Submit</button>
-        
+
+      </form>
+    </section>
+  );
+}"use client";
+import "./Contact.css";
+
+export default function Contact() {
+  return (
+    <section id="contact" className="contactSection">
+      <div className="line"/>
+      <h1 className="contactHeading">Contact Me</h1>
+
+      <form 
+        name="contact" 
+        method="POST" 
+        data-netlify="true" 
+        netlify-honeypot="bot-field"
+        className="contactForm"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" />
+          </label>
+        </p>
+
+        <input type="text" name="name" placeholder="Name" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <textarea name="message" placeholder="Message" rows="5" required />
+        <button type="submit" className="contactButton">Submit</button>
       </form>
     </section>
   );
 }
+
